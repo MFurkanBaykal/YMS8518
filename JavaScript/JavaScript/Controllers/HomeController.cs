40c1db218d0a8c5c130952bd0f656795b9304179
Furkan <mffurkanbaykal@hotmail.com>
2019-11-24 17:39:01 +0300

    Javascript çıktı üretme #1

---
+++ JavaScript/JavaScript/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace JavaScript.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Javascript1()
+        {
+            return View();
+        }
+    }
+}
